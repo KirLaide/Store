ddd79c30e4a394ead4816690cc7c499e14825b4c
KirLaide <yurii.disnenko@gmail.com>
2016-02-29 11:10:00 +0300

    add_Category_Controller_and_View

--- SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -28,7 +28,10 @@ namespace SportsStore.WebUI.Controllers
                 .OrderBy(p => p.ProductID)
                 .Skip((page - 1) * PageSize)
                 .Take(PageSize),
-                PagingInfo = new PagingInfo { CurrentPage = page, ItemsPerPage = PageSize, TotalItems = repository.Products.Count() },
+                PagingInfo = new PagingInfo {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = category == null ? repository.Products.Count() : repository.Products.Where(e=>e.Category == category).Count() },
                 CurrentCategory = category
             };
             return View(model);
